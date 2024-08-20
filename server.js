54411552d71cfbb03814e916058d27957cb1fb5b
Lina Aparicio <laparici9@gmail.com>
2024-08-20 22:19:19 +0000

    port changed

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ wsServer.initialize(server);
 app.use('/api', bingoRoutes, authRoutes);
 
 // Server configuration
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 server.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
